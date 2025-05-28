bce3c1f369d82e0b865c482ed21b12407eacd23c
Pedro Martins <pedrohenriquemgil2007@gmail.com>
2025-05-28 17:00:49 -0300

    Primeiro commit

---
+++ js/myjs.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const texto = "Eu sou";
+    let index = 0;
+    let deletando = false;
+    const elemento = document.getElementById("spanver");
+
+    function escrever() {
+        if (!deletando && index < texto.length) {
+            elemento.innerHTML += texto.charAt(index);
+            index++;
+            setTimeout(escrever, 200);
+        } else if (deletando && index > 0) {
+            elemento.innerHTML = texto.substring(0, index - 1);
+            index--;
+            setTimeout(escrever, 100);
+        } else {
+            deletando = !deletando;
+            setTimeout(escrever, 1000); // Tempo de espera antes de recomeçar
+        }
+    }
+
+    escrever();
+});
